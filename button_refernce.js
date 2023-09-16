9e3f6ee2342ebc1dba4404abcd27d93295ad8a39
devrat1144 <137684298+devrat1144@users.noreply.github.com>
2023-09-16 22:20:17 +0530

    Add files via upload

---
+++ button_refernce.js
@@ -0,0 +1,31 @@
+
+function scrollFunction1() {
+    let e = document.getElementById("cardsid");
+    e.scrollIntoView({
+      block: 'start',
+      behavior: 'smooth',
+      inline: 'center'
+    });
+  }
+  function scrollFunction2() {
+    let e = document.getElementById("certificateid");
+    e.scrollIntoView({
+      block: 'start',
+      behavior: 'smooth',
+      inline: 'center'
+    });
+  }function scrollFunction3() {
+    let e = document.getElementById("aboutid");
+    e.scrollIntoView({
+      block: 'start',
+      behavior: 'smooth',
+      inline: 'center'
+    });
+  }function scrollFunction4() {
+    let e = document.getElementById("contactid");
+    e.scrollIntoView({
+      block: 'start',
+      behavior: 'smooth',
+      inline: 'center'
+    });
+  }
